f2230dfdc54227f0ff021e54590fd88bc80b860b
grantmwilliams <grant.williams@okstate.edu>
2018-03-08 14:33:27 -0600

    cleaning up old model attempt

--- bwt.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Grant Williams on 8/23/16.
-//
-
-#ifndef GCOMP_BWT_H
-#define GCOMP_BWT_H
-
-#endif //GCOMP_BWT_H
--- mtf.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Grant Williams on 8/23/16.
-//
-
-#ifndef GCOMP_MTF_H
-#define GCOMP_MTF_H
-
-#endif //GCOMP_MTF_H
